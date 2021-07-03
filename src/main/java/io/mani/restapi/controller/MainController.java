edf3b6854a852fab538fa455a53fca396e0798c7
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-03 22:23:56 +0200

    add controller more

--- src/main/java/io/mani/restapi/controller/MainController.java
+++ src/main/java/io/mani/restapi/controller/MainController.java
@@ -1,5 +1,6 @@
 package io.mani.restapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,28 +10,13 @@ import java.util.List;
 @RestController
 public class MainController {
 
+    @Autowired
+    private TopicService topicService;
+
+
     @RequestMapping("/topics")
-    public List<Topic> HomePage() {
-        return Arrays.asList(
-                new Topic("1", "Angular", "A Good Framework to handle Front end"),
-                new Topic("2", "React", "A Good Facebook Framework to handle Front end"),
-                new Topic("3", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
-                new Topic("4", "Node Js", "The best way to handle backend fast reliable and secure "),
-                new Topic("1", "Angular", "A Good Framework to handle Front end"),
-                new Topic("2", "React", "A Good Facebook Framework to handle Front end"),
-                new Topic("3", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
-                new Topic("4", "Node Js", "The best way to handle backend fast reliable and secure "),
-                new Topic("1", "Angular", "A Good Framework to handle Front end"),
-                new Topic("2", "React", "A Good Facebook Framework to handle Front end"),
-                new Topic("3", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
-                new Topic("4", "Node Js", "The best way to handle backend fast reliable and secure "),
-                new Topic("1", "Angular", "A Good Framework to handle Front end"),
-                new Topic("2", "React", "A Good Facebook Framework to handle Front end"),
-                new Topic("3", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
-                new Topic("4", "Node Js", "The best way to handle backend fast reliable and secure ")
-
-
-        );
+    public List<Topic> TopicController() {
+           return
     }
 
     @RequestMapping("/about")
---
+++ src/main/java/io/mani/restapi/controller/TopicService.java
@@ -0,0 +1,22 @@
+package io.mani.restapi.controller;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("1", "Angular", "A Good Framework to handle Front end"),
+            new Topic("2", "React", "A Good Facebook Framework to handle Front end"),
+            new Topic("3", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
+            new Topic("4", "Node Js", "The best way to handle backend fast reliable and secure "),
+
+            );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+}
