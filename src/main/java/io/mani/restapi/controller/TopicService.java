5770d59136bb9b2426f3b34b7a84fe2c8068116e
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-04 13:10:51 +0200

    bug solved

--- src/main/java/io/mani/restapi/controller/TopicService.java
+++ src/main/java/io/mani/restapi/controller/TopicService.java
@@ -3,13 +3,14 @@ package io.mani.restapi.controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("angular", "Angular course", "A Good Framework to handle Front end"),
             new Topic("react", "React  boot camp", "A Good Facebook Framework to handle Front end"),
             new Topic("spring", "Spring Boot", "A Good Framework to handle back end for enterprise apps"),
@@ -17,7 +18,7 @@ public class TopicService {
             new Topic("nest", "Nest Js", "The out of the Box handle backend solution "),
             new Topic("svelte", "Svelte", "The Future if front end technology ")
 
-    );
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
